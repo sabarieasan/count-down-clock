4bfeaafce254763074648b4c226c15095a957fbd
sabarieasan <sabarishglad28@gmail.com>
2023-08-04 15:10:26 +0530

    countdown-clock

---
+++ script.js
@@ -0,0 +1,34 @@
+const tillDate = new Date(2023, 8, 15, 12, 0, 0);
+
+function countdowns() {
+  const currentDate = new Date();
+  const timeDifference = tillDate - currentDate;
+
+  if (timeDifference <= 0) {
+    document.getElementById("days").innerText = "00";
+    document.getElementById("hours").innerText = "00";
+    document.getElementById("minutes").innerText = "00";
+    document.getElementById("seconds").innerText = "00";
+    return;
+  }
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = formatTime(days);
+  document.getElementById("hours").innerText = formatTime(hours);
+  document.getElementById("minutes").innerText = formatTime(minutes);
+  document.getElementById("seconds").innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countdowns();
+
+setInterval(countdowns, 1000);
